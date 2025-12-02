51fb638d8cd66e0b1cc702bc4b63bc772fa11faa
marshal08 <lubomir.beluch@gmail.com>
2025-12-02 21:45:12 +0100

    Update sensor.py

--- esphome/components/sthspresence/sensor.py
+++ esphome/components/sthspresence/sensor.py
@@ -1,15 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_NAME
+from esphome.const import CONF_ID
 
 sthspresence_ns = cg.esphome_ns.namespace("sthspresence")
-STHS34PF80Sensor = sthspresence_ns.class_("STHS34PF80Sensor", sensor.Sensor)
+STHS34PF80Sensor = sthspresence_ns.class_("STHS34PF80Sensor", cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(STHS34PF80Sensor).extend({
-    cv.Required(CONF_NAME): cv.string,
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(CONF_ID): cv.declare_id(STHS34PF80Sensor),
+    cv.Optional("temperature"): sensor.sensor_schema(),
+    cv.Optional("presence"): sensor.sensor_schema(),
+    cv.Optional("motion"): sensor.sensor_schema(),
 })
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_NAME])
-    yield sensor.register_sensor(var, config)
