1fbab4155cec15217cba4ffc2f502b8107252915
marshal08 <lubomir.beluch@gmail.com>
2025-12-02 21:36:40 +0100

    Update sensor.py

--- components/sthspresence/sensor.py
+++ components/sthspresence/sensor.py
@@ -1,61 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import (
-    UNIT_CELSIUS,
-    UNIT_EMPTY,
-    ICON_THERMOMETER,
-    CONF_ID,
-)
+from esphome.const import CONF_NAME
 
-DEPENDENCIES = ["i2c"]
-AUTO_LOAD = ["sensor"]
+sthspresence_ns = cg.esphome_ns.namespace("sthspresence")
+STHS34PF80Sensor = sthspresence_ns.class_("STHS34PF80Sensor", sensor.Sensor)
 
-# Config keys
-CONF_TEMPERATURE = "temperature"
-CONF_PRESENCE = "presence"
-CONF_MOTION = "motion"
-
-# Namespace and class binding
-sth_ns = cg.esphome_ns.namespace("sthspresence")
-STHS34PF80Sensor = sth_ns.class_("STHS34PF80Sensor", cg.PollingComponent)
-
-# Schema definition
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(STHS34PF80Sensor),
-
-    cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-        unit_of_measurement=UNIT_CELSIUS,
-        icon=ICON_THERMOMETER,
-        accuracy_decimals=1,
-    ),
-
-    cv.Optional(CONF_PRESENCE): sensor.sensor_schema(
-        unit_of_measurement=UNIT_EMPTY,
-        icon="mdi:account",
-        accuracy_decimals=1,
-    ),
-
-    cv.Optional(CONF_MOTION): sensor.sensor_schema(
-        unit_of_measurement=UNIT_EMPTY,
-        icon="mdi:run",
-        accuracy_decimals=1,
-    ),
+CONFIG_SCHEMA = sensor.sensor_schema(STHS34PF80Sensor).extend({
+    cv.Required(CONF_NAME): cv.string,
 })
 
-# Code generation
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
-    if CONF_TEMPERATURE in config:
-        sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
-        cg.add(var.set_temperature_sensor(sens))
-
-    if CONF_PRESENCE in config:
-        sens = await sensor.new_sensor(config[CONF_PRESENCE])
-        cg.add(var.set_presence_sensor(sens))
-
-    if CONF_MOTION in config:
-        sens = await sensor.new_sensor(config[CONF_MOTION])
-        cg.add(var.set_motion_sensor(sens))
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_NAME])
+    yield sensor.register_sensor(var, config)
