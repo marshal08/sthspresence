c56800def36ceabc2affe29929748db525156ed2
marshal08 <lubomir.beluch@gmail.com>
2025-12-03 20:46:31 +0100

    Update sensor.py

--- esphome/components/sthspresence/sensor.py
+++ esphome/components/sthspresence/sensor.py
@@ -1,16 +1,16 @@
-# esphome/components/sthspresence/sensor.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
 
-# Namespace matches C++ namespace below
 sthspresence_ns = cg.esphome_ns.namespace("sthspresence")
 STHS34PF80Sensor = sthspresence_ns.class_("STHS34PF80Sensor", cg.PollingComponent)
 
+# Optional ambient temperature sub-sensor included
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_ID): cv.declare_id(STHS34PF80Sensor),
-    cv.Optional("temperature"): sensor.sensor_schema(),
+    cv.Optional("object_temperature"): sensor.sensor_schema(),
+    cv.Optional("ambient_temperature"): sensor.sensor_schema(),
     cv.Optional("presence"): sensor.sensor_schema(),
     cv.Optional("motion"): sensor.sensor_schema(),
 })
@@ -19,9 +19,13 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
 
-    if "temperature" in config:
-        sens = yield sensor.new_sensor(config["temperature"])
-        cg.add(var.set_temperature_sensor(sens))
+    if "object_temperature" in config:
+        sens = yield sensor.new_sensor(config["object_temperature"])
+        cg.add(var.set_object_temperature_sensor(sens))
+
+    if "ambient_temperature" in config:
+        sens = yield sensor.new_sensor(config["ambient_temperature"])
+        cg.add(var.set_ambient_temperature_sensor(sens))
 
     if "presence" in config:
         sens = yield sensor.new_sensor(config["presence"])
