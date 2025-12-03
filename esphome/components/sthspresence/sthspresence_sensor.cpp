831b8186a8ffbd12a1643fc6c220e23dba143302
marshal08 <lubomir.beluch@gmail.com>
2025-12-03 21:00:28 +0100

    Update sthspresence_sensor.cpp

--- esphome/components/sthspresence/sthspresence_sensor.cpp
+++ esphome/components/sthspresence/sthspresence_sensor.cpp
@@ -1,8 +1,7 @@
-// esphome/components/sthspresence/sthspresence_sensor.cpp
 #include "sthspresence_sensor.h"
 
 namespace esphome {
 namespace sthspresence {
-// All methods are defined inline in the header.
+// Implementation is inline in the header.
 }  // namespace sthspresence
 }  // namespace esphome
